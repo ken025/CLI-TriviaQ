3d686632b041e4bcdc187b5f0158652943f1e263
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-07 20:19:04 +0000

    Done.

---
+++ api.rb
@@ -0,0 +1,16 @@
+require 'rest-client'
+require 'json'
+require 'pry'
+
+
+
+
+resp = RestClient.get('https://opentdb.com/api.php?amount=15&category=9&difficulty=medium&type=multiple')
+
+trivia_hash = JSON.parse(resp.body, symbolize_names:true)
+
+trivia_arr = trivia_hash[:results]
+
+trivia_questions = trivia_arr.collect do |question|
+  Trivia.new(question)
+end 
---
+++ trivia.rb
@@ -0,0 +1,7 @@
+require_relative 'trivia'
+require 'rest-client'
+require 'json'
+require 'pry'
+
+
+https://opentdb.com/api.php?amount=15&category=9&difficulty=medium&type=multiple
