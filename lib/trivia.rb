8bd35b1b24f6c877dcf4c20e75738d277b506d6c
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-09 20:48:25 +0000

    Done.

--- lib/trivia.rb
+++ lib/trivia.rb
@@ -3,16 +3,21 @@ require 'pry'
 class Trivia
   @@all = []
   
-  attr_accessor :question
+  attr_accessor :question, :correct_answer, :incorect_answers, :total_answers
   
   def initialize(q)
-    self.question
+     self.question = q[:question]
+     self.correct_answer = q[:correct_answer]
+     self.incorect_answers = q[:incorect_answers]
+     self.total_answers << correct_answer 
+     self.total_answers << incorect_answers
     @@all << self 
   end 
   
   def self.all 
     @@all 
   end 
+  
 end 
 
 
