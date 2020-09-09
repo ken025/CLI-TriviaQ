cd30c8e86a15c13e7191a43cf8f93f0e97e483ad
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-09 14:03:45 +0000

    Done.

--- api.rb
+++ api.rb
@@ -3,13 +3,12 @@ require 'json'
 require 'pry'
 
 
-
-
 resp = RestClient.get('https://opentdb.com/api.php?amount=15&category=9&difficulty=medium&type=multiple')
 
 trivia_hash = JSON.parse(resp.body, symbolize_names:true)
 
 trivia_arr = trivia_hash[:results]
+quest_arr = trivia_arr.collect { |x| x[:question] }
 
 binding.pry 
 
--- trivia.rb
+++ trivia.rb
@@ -16,3 +16,5 @@ class Trivia
   
   binding.pry
 end 
+
+
