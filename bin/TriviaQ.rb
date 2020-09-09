fa7c474e5ad63102f28e7184d9e0f1bee9b42f2e
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-09 18:52:19 +0000

    Done.

---
+++ bin/TriviaQ.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby 
+
+require_relative 'trivia'
+require 'rest-client'
+require 'json'
+require 'pry'
--- lib/api.rb
+++ lib/api.rb
@@ -9,6 +9,7 @@ resp = RestClient.get('https://opentdb.com/api.php?amount=15&category=9&difficul
 trivia_hash = JSON.parse(resp.body, symbolize_names:true)
 
 trivia_arr = trivia_hash[:results]
+quest_arr = trivia_arr.collect { |x| x[:question] }
 
 quest_arr = trivia_arr.collect { |questions| questions[:question]}
     Trivia.new(questions)
