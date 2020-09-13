f22043a8919dcac469d1ad08f1a2eac5d75b56de
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-13 18:28:17 +0000

    Done.

--- lib/api.rb
+++ lib/api.rb
@@ -4,8 +4,7 @@ def self.get_questions
 resp = RestClient.get('https://opentdb.com/api.php?amount=15&category=9&difficulty=medium&type=multiple')
 
 trivia_hash = JSON.parse(resp.body, symbolize_names:true)
-trivia_arr = trivia_hash[:results]
-binding.pry  
+trivia_arr = trivia_hash[:results]  
  
 triv_ques = trivia_arr.collect do | q |
     Trivia.new(q)
